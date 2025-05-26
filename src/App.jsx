8848d9f019fc85f28a4f3c3d68bff8303b709288
Osama Javed <osamajaved2002@gmail.com>
2025-05-27 04:12:51 +0500

    27-05-2025

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarMain from "./components/navbar/NavbarMain";
+
+const App = () => {
+  return (
+    <main className="font-body">
+      <NavbarMain />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavbarLogo = () => {
+  return (
+    <div>
+      <h1 className="text-white text-2xl ">Osama Javed</h1>
+    </div>
+  );
+};
+
+export default NavbarLogo;
