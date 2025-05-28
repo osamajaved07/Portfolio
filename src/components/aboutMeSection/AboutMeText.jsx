beaf439dd8dd207bfa71e3ceb523be431b7222b7
Osama Javed <www.om173116@gmail.com>
2025-05-29 02:05:29 +0500

    Office

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const AboutMeText = () => {
   return (
-    <div>
-      <h2>About ME</h2>
-      <p></p>
+    <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
+      <h2 className="text-6xl text-cyan mb-10">About ME</h2>
+      <p>I’m Osama, a web developer with a passion for coding. I specialize in React and front-end development, helping
+        clients build real-world projects and get modern web technologies. Outside of coding, I enjoy continuous learning and
+        sharing knowledge to inspire others to achieve their goals.</p>
+        <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
+          My Projects
+        </button>
     </div>
   );
 };
