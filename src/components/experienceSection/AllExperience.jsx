86bbdd51b1cc87175a7745ab9d93096a84e28e56
Osama Javed <www.om173116@gmail.com>
2025-06-02 22:05:27 +0500

    Office

--- src/components/experienceSection/AllExperience.jsx
+++ src/components/experienceSection/AllExperience.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { FaArrowRightLong } from "react-icons/fa6";
+import SingleExperience from "./SingleExperience";
+
+const experiences = [
+  {
+    job: "Front-End Developer",
+    company: "Alex Apps",
+    date: "2022 - Present",
+    responsibilities: [
+      "Implementing reusable components.",
+      "Participating in large scale application.",
+      "Working on the performance of web applications.",
+      "Generating new ideas for better user experience.",
+    ],
+  },
+  {
+    job: "Course Instructor",
+    company: "Nucamp",
+    date: "2023 - Present",
+    responsibilities: [
+      "Explaining and facilitating web development concepts.",
+      "Help students with their assignments and grade them weekly.",
+      "Provide support for students through their learning journey.",
+    ],
+  },
+  {
+    job: "Course Instructor",
+    company: "Sprints",
+    date: "2024 - Present",
+    responsibilities: [
+      "Teaching JavaScript, React and TailwindCSS.",
+      "Participating in preparing course materials.",
+      "Helping students through their way in learning web development technologies.",
+    ],
+  },
+];
 
 const AllExperience = () => {
   return (
-    <div>AllExperience</div>
-  )
-}
+    <div className="flex md:flex-row sm:flex-col items-center justify-between">
+      {experiences.map((experience, index) => {
+        return (
+          <>
+            <SingleExperience key={index} experience={experience} />;
+            {index < 2 ? (
+              <FaArrowRightLong className="text-6xl text-orange lg:block sm:hidden" />
+            ) : (
+              ""
+            )}
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
-export default AllExperience
+export default AllExperience;
