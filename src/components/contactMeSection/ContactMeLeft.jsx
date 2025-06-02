dc7accb23f5096bdfbbd24b2d7500ac0e6f054d8
Osama Javed <www.om173116@gmail.com>
2025-06-03 01:05:38 +0500

    Office

--- src/components/contactMeSection/ContactMeLeft.jsx
+++ src/components/contactMeSection/ContactMeLeft.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import ContactForm from "./ContactForm";
+import ContactText from "./ContactText";
 
 const ContactMeLeft = () => {
-  return <div>ContactMeLeft</div>;
+  return (
+    <div className="flex flex-col gap-8 w-full">
+      <ContactText />
+      <ContactForm />
+    </div>
+  );
 };
 
 export default ContactMeLeft;
