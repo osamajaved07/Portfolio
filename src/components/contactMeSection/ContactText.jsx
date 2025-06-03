8930f09ab53019b2ad6e19baf8e0ca882c12f79d
Osama Javed <www.om173116@gmail.com>
2025-06-04 01:43:39 +0500

    Office

--- src/components/contactMeSection/ContactText.jsx
+++ src/components/contactMeSection/ContactText.jsx
@@ -2,7 +2,7 @@ const ContactText = () => {
   return (
     <div>
       <h2 className="text-orange text-3xl mb-4">Get In Touch</h2>
-      <p>
+      <p className="text-white">
         Feel free to reach out if you’d like to collaborate
         <br />
         you are just a few clicks away!
