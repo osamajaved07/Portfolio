68d99aefe76e19e0c1bd1a31adb0fde279c931eb
Osama Javed <osamajaved2002@gmail.com>
2025-06-16 01:55:05 +0500

    footer section done

--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 const HeroText = () => {
   return (
     <div className="flex flex-col gap-4 h-full justify-center md:text-left sm:text-center">
-      <h2 className="lg:text-2xl sm:text-xl  uppercase text-lightGrey ">Front-End Mobile and Web Developer</h2>
-
-      <h1 className="md:text-[2.8rem] lg:text-6xl sm:text-4xl text-orange font-bold uppercase">Osama Javed</h1>
-
-      <p className="text-lg mt-4 text-white">A passionate Mobile and web developer</p>
+      <motion.h2 variants={fadeIn("down", 0.2)} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0 }} className="lg:text-2xl sm:text-xl  uppercase text-lightGrey ">
+        Front-End Web and Mobile Application Developer
+      </motion.h2>
+      <motion.h1 variants={fadeIn("right", 0.4)} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0 }} className="md:text-[2.8rem] lg:text-6xl sm:text-4xl text-orange font-bold uppercase">
+        Osama Javed
+        {/* <br className="sm:hidden md:block" /> */}
+      </motion.h1>
+      <motion.p variants={fadeIn("up", 0.6)} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0 }} className="text-lg mt-4 text-white">
+        A Passionate Web and App Developer <br /> with 1 years of experience.
+      </motion.p>
     </div>
   );
 };
